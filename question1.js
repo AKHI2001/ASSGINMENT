045fcd2f589332cee6b05a02f602346db744f26f
Akhilesh kumar maurya <94899077+AKHI2001@users.noreply.github.com>
2021-12-31 17:05:23 +0530

    Add files via upload

---
+++ question1.js
@@ -0,0 +1,11 @@
+var a = prompt("enter first number");
+var b = prompt("enter second number");
+var c = prompt("enter third number");
+if(a<=b && a<=c){
+     alert(a+ "is smaller");
+    }
+   else if(b<=a && b<=c){
+     alert(b+ "is smaller");
+   } 
+   else
+     alert(c+ "is smaller");

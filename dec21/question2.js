60ce225713ee5d8ce418e6f03d4aac49a269b421
Akhilesh kumar maurya <94899077+AKHI2001@users.noreply.github.com>
2021-12-31 17:47:31 +0530

    Update question2.js

--- dec21/question2.js
+++ dec21/question2.js
@@ -1,9 +1,9 @@
 var a=prompt("enter first number");
 var b=prompt("enter second number");
 var c=prompt("enter third number");
-if(a>=b && a>=c)
+if(a>b && a>c)
      alert(a+ "is greater");
-   else if(b>=a && b>=c)
+   else if(b>a && b>c)
      alert(b+ "is greater"); 
    else
      alert(c+ "is greater"); 

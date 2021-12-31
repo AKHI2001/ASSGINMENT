e95b1117085fff8361d073b4308dfae4c9213e5a
Akhilesh kumar maurya <94899077+AKHI2001@users.noreply.github.com>
2021-12-31 14:34:52 +0530

    Add files via upload

---
+++ dec21/question2.js
@@ -0,0 +1,9 @@
+var a=prompt("enter first number");
+var b=prompt("enter second number");
+var c=prompt("enter third number");
+if(a>=b && a>=c)
+     alert("a is greater than both b and c");
+   else if(b>=a && b>=c)
+     alert("b is less than both a and c"); 
+   else
+     alert("c is greater than both a and b"); 

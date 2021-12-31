f6747e310389108c40b56fcedda38db291f91263
Akhilesh kumar maurya <94899077+AKHI2001@users.noreply.github.com>
2021-12-31 18:41:48 +0530

    Update question1.js

--- dec23/question1.js
+++ dec23/question1.js
@@ -1,36 +1,42 @@
-var students =prompt("enter name student");
-var marks=prompt("enter marks of student");
-if(marks>90)
+var g_class = {
+    'name': "DSMNRU",
+    'instructor': {
+      'name': "Dinesh singh",
+      'subject': "thermodynamic"
+    },
+    'students': [{
+        'name': 'rahul',
+        'marks': 80,
+      },
+      {
+        'name': 'deepika singh',
+        'marks': 89,
+      },
+      {
+        'name': 'akhilesh',
+        'marks': 77,
+      },
+      {
+        'name': 'Ayush',
+        'marks': 70,
+      },
+      {
+       'name': 'hena',
+       'marks': 90,
+      },
+      {
+       'name': 'ganesh gupta',
+       'marks': 85,
+      },
+      {
+       'name': 'yogesh',
+       'marks': 87,
+      }
+    ],
+  };
+  for(var i=0; i<g_class.students.length;i++)
   {
-  alert("you have passed");
+      if(g_class.students[i].marks > 85){
+          console.log(g_class.students[i].name);
+        }
   }
- else if(90>marks<70)
-  {
-  alert("you passed second division");
-  }
- else("you passed third division");
-   console.log(marks);
-var students =prompt("enter name student");
-var marks=prompt("enter marks of student");
-if(marks>90)
-  {
-  alert("you have passed");
-  }
- else if(marks<70)
-  {
-  alert("you passed second division");
-  }
- else("you passed third division");
-   console.log(marks);
-var students =prompt("enter name student");
-var marks=prompt("enter marks of student");
-if(marks>90)
-  {
-  alert("you have passed");
-  }
- else if(marks<70)
-  {
-  alert("you passed second division");
-  }
- else("you passed third division");
-   console.log(marks);
